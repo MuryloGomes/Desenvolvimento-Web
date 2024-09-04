492cb5b0c0bc9644043be8c6f272d394cd8eaf12
Murylo Gomes Alves <muryloalves15@gmail.com>
2024-09-04 14:37:29 -0300

    criando login js

---
+++ LOGIN/login.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Login realizado com sucesso!');
+    // Adicione aqui a lógica para autenticação do usuário
+});
