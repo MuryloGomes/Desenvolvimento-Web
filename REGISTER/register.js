340e8e7829e6956b064b4c8975e4e17ad4c864c0
joaovitor_sg <joaovitorsimao13ff@gmail.com>
2024-09-05 19:21:16 -0300

    ultima do dia

--- REGISTER/register.js
+++ REGISTER/register.js
@@ -1,5 +1,5 @@
 document.querySelector('form').addEventListener('submit', function(e) {
     e.preventDefault();
     alert('Cadastro realizado com sucesso!');
-    // Adicione aqui a lógica para registrar o usuário
+    
 });
