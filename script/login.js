eb82f5615abe522bad2f3805deb36f661e2159ce
Allan Rocha <allangvrocha04@gmail.com>
2024-10-12 17:03:03 -0300

    inicio login

--- script/login.js
+++ script/login.js
@@ -1,40 +1,25 @@
-const url = "https://go-wash-api.onrender.com/api/login"; // Substitua pelo URL correto da API de login
+const url = "https://go-wash-api.onrender.com/api/login";
 
-async function loginUsuario() {
-    const email = document.getElementById('email').value;
-    const senha = document.getElementById('senha').value;
-
-    try {
-        let response = await fetch(url, {
-            method: "POST",
-            body: JSON.stringify({
-                "email": email,
-                "password": senha
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (response.ok) {
-            let data = await response.json();
-            console.log('Login bem-sucedido:', data);
-            localStorage.setItem('usuario', JSON.stringify(data)); // Armazena os dados do usuário no localStorage
-            alert('Login realizado com sucesso!');
-            // Redireciona para uma página protegida ou o dashboard
-            window.location.href = 'dashboard.html'; // Substitua pelo URL da página protegida
-        } else {
-            let errorData = await response.json();
-            console.error('Erro ao fazer login:', errorData);
-            alert(`Erro ao fazer login: ${errorData.message || 'Verifique os dados e tente novamente.'}`);
+async function login() {
+    let api = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            "email": "jiajing6623@uorak.com",
+            "user_type_id": 1,
+            "password": "123456781"
+        }),
+        headers: {
+            "Content-Type": "application/json"
         }
-    } catch (error) {
-        console.error('Erro na requisição:', error);
-        alert('Ocorreu um erro ao tentar fazer login. Por favor, tente novamente.');
+    });
+
+    if (api.ok) {
+        let resposta = await api.json();
+        console.log(resposta);
+        return;
     }
+
+    alert("Erro");
 }
 
-document.getElementById('loginForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    loginUsuario();
-});
+login();
