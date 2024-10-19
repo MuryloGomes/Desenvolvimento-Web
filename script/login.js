c51a0f71cd58373358a0c580ad88ab2771566ca3
Murylo Gomes Alves <muryloalves15@gmail.com>
2024-10-18 21:30:16 -0300

    login 100 token

--- script/login.js
+++ script/login.js
@@ -1,50 +1,47 @@
 const url = 'https://go-wash-api.onrender.com/api/login';
-const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodtZWZjOWM5NTgyNjg3Lmhlcm9rdWFwcC5jb20vYXBpL2xvZ2luIiwiaWF0IjoxNzEyMDc4Mjg0LCJuYmYiOjE3MTIwNzgyODQsImp0aSI6ImRPajVkTng4WEgxdVJ5TVkiLCJzdWIiOiIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3M";
 
-async function loginUser(){
-    event.preventDefault();
+async function loginUser(event) {
+    event.preventDefault(); 
     document.getElementById('loader').style.display = 'block';
-    let email = document.getElementById('email').value
-    let password = document.getElementById('password').value
 
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('senha').value; 
+
+    try {
         let api = await fetch(url, {
-            method:"POST",
-            body:JSON.stringify({
-            "email":email,
-            "password": password,
-            "user_type_id":1
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-             "Authorization": `Bearer ${token}`
-        }
-    });
+            method: "POST",
+            body: JSON.stringify({
+                "email": email,
+                "password": password,
+                "user_type_id": 1 
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
-    let resposta = await api.json();
-    // Esconde o carregador após a resposta
-    document.getElementById('loader').style.display = 'none';
+        let resposta = await api.json();
+        document.getElementById('loader').style.display = 'none';
 
-    if (api.ok){
-        alert("Sucesso")
-        if (resposta.access_token) {
-            localStorage.setItem("accessToken", resposta.access_token);
-            localStorage.setItem('loggedIn', true); // Adicione esta linha
-        }
+        if (api.ok) {
+            alert("Sucesso");
+            if (resposta.access_token) {
+                localStorage.setItem("accessToken", resposta.access_token);
+                localStorage.setItem('loggedIn', true);
+            }
 
-        if (resposta.user) {
-            localStorage.setItem("user", JSON.stringify(resposta.user));
+            if (resposta.user) {
+                localStorage.setItem("user", JSON.stringify(resposta.user));
+            }
+            window.location.href = "index.html";
+        } else {
+            alert(resposta.data?.errors ? resposta.data.errors.join(", ") : "Erro desconhecido");
         }
-        window.location.href = "index.html";
-    }
-    else{
-        alert(resposta.data.errors);
+    } catch (error) {
+        document.getElementById('loader').style.display = 'none';
+        alert("Erro ao tentar logar. Tente novamente mais tarde.");
+        console.error('Erro:', error);
     }
 }
 
 document.getElementById('loginForm').addEventListener('submit', loginUser);
-
-
-
-
-
-
